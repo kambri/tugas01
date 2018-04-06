865c69b8bee8086a4e3fc927e5c229554894e139
kambri <37506225+kambri@users.noreply.github.com>
2018-04-06 21:22:18 +0700

    Instance Object

--- src/tugas01/MahasiswaAksi.java
+++ src/tugas01/MahasiswaAksi.java
@@ -11,6 +11,8 @@ package tugas01;
  */
 public class MahasiswaAksi {
     public static void main(String[] args) {
-        
+        Mahasiswa Kambri = new Mahasiswa();
+        Mahasiswa Suci = new Mahasiswa();
+        Mahasiswa Andi = new Mahasiswa();
     }
 }
