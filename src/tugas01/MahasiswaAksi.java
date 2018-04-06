7bd4b37488c3d4065669042d65f105563198670c
kambri <37506225+kambri@users.noreply.github.com>
2018-04-06 21:27:40 +0700

    Mengisi Atribut

--- src/tugas01/MahasiswaAksi.java
+++ src/tugas01/MahasiswaAksi.java
@@ -14,5 +14,23 @@ public class MahasiswaAksi {
         Mahasiswa Kambri = new Mahasiswa();
         Mahasiswa Suci = new Mahasiswa();
         Mahasiswa Andi = new Mahasiswa();
+        
+        Kambri.nama="M.Kambrianor";
+        Kambri.npm="16630623";
+        Kambri.kelas="Non Reguler BJB 4A";
+        Kambri.jurusan="Teknik Informatika";
+        Kambri.ipk=3.8;
+        
+        Suci.nama="Suciani";
+        Suci.npm="16630893";
+        Suci.kelas="Non Reguler BJB 4B";
+        Suci.jurusan="Teknik Informatika";
+        Suci.ipk=3.5;
+        
+        Andi.nama="Andiansyah";
+        Andi.npm="16638899";
+        Andi.kelas="Non Reguler BJB 4C";
+        Andi.jurusan="Teknik Informatika";
+        Andi.ipk=3.0;
     }
 }
